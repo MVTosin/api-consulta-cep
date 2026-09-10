276607f0b9f675fd7986774c031fbe618a2d9d2a
Tosin <marcostosin@kpmg.com.br>
2026-09-10 16:41:15 -0300

    feat: adicionar tratamento de caracteres especiais

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 def validar_cpf(cpf):
-	return len(cpf) == 11
+    cpf = cpf.replace(".", "").replace("-", "")
+
+    if not cpf.isdigit():
+        return False
+
+    return len(cpf) == 11
+
 
 cpf = input("Digite o CPF: ")
 
 if validar_cpf(cpf):
-	print("CPF válido")
-
+    print("CPF válido")
 else:
-	print("CPF inválido")
+    print("CPF inválido")
